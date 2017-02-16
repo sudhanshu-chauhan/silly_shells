034fcc34b93491f8dfcc6139a17584bf45462406
sudhanshu singh chauhan <minimalcrap@gmail.com>
2017-02-16 22:58:04 +0530

    add logger.py

---
+++ components/common_utils/logger.py
@@ -0,0 +1,50 @@
+import logging
+import os
+
+from components.conf import settings
+
+
+class Logger:
+
+    def __init__(self,
+                 file_name=None,
+                 log_dir=None,
+                 stream_handler=None,
+                 file_handler=None
+                 ):
+        """
+
+        :param file_name:
+        :param log_dir:
+        :param stream_handler:
+        :param file_handler:
+        """
+        try:
+        	
+            if file_name is None:
+                raise Exception("log file name not defined")
+
+            if log_dir is None:
+                self.log_dir = settings.LOG_DIR
+            else:
+                self.log_dir = log_dir
+
+            self.logger = logging.getLogger(name=file_name)
+            log_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            self.logger.setLevel(logging.DEBUG)
+
+            if file_handler:
+                self.file_handler = logging.FileHandler(os.path.join(self.log_dir, file_name))
+                self.file_handler.setFormatter(log_format)
+                self.file_handler.setLevel(logging.DEBUG)
+                self.logger.addHandler(self.file_handler)
+
+            if stream_handler:
+                self.stream_handler = logging.StreamHandler()
+                self.stream_handler.setFormatter(log_format)
+                self.stream_handler.setLevel(logging.DEBUG)
+                self.logger.addHandler(self.stream_handler)
+
+
+        except Exception as err:
+            print "logger::init: {}".format(err.message)
