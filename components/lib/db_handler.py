6236745c6dbce372a22cf9774e9743035045093e
Sudhanshu Singh Chauhan <minimalcrap@gmail.com>
2017-07-30 00:23:34 +0530

    feat.(silly_shells): modify db_handler.py to add further models functions

--- components/lib/db_handler.py
+++ components/lib/db_handler.py
@@ -3,7 +3,7 @@ import uuid
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-from models import User
+from models import User, Token, SecurityCredential
 
 
 database_url = 'postgresql://postgres:random123@localhost/silly_shells'
@@ -13,15 +13,14 @@ Session = sessionmaker(bind=engine)
 
 
 class HandleDB:
-    """
-    HandleDB class for handling db interactions.
+    """HandleDB class for handling db interactions.
     """
 
     def __init__(self):
         """
         HandleDB __init__ method.
         """
-        pass
+        self.session_instance = Session()
 
     def create_user(self, **user_params):
         """Create User method for HandleDB class
@@ -33,10 +32,49 @@ class HandleDB:
         try:
             user_params['uuid'] = str(uuid.uuid4())
             current_user = User(**user_params)
-            session_instance = Session()
-            session_instance.add(current_user)
-            session_instance.commit()
+            self.session_instance.add(current_user)
+            self.session_instance.commit()
             return True
         except Exception as error:
-            print(error.message)
+            print('HandleDB::create_user:{}'.format(error.message))
+            return False
+
+    def create_security_credential(self, **secureity_credential_params):
+        """Create Security Credential method for HandleDB Class.
+
+        Args:
+            security_credential_params (Dict): multi key, value param
+            for security credential object creation.
+
+        """
+        try:
+            secureity_credential_params['uuid'] = str(uuid.uuid4())
+            security_credential_instance = SecurityCredential(
+                **secureity_credential_params
+            )
+            self.session_instance.add(security_credential_instance)
+            self.session_instance.commit()
+            return True
+
+        except Exception as error:
+            print('HandleDB::create_security_credential:{}'.format(
+                error.message
+            ))
+            return False
+
+    def create_token(self, **token_params):
+        """Create Token method for HandleDB class.
+
+        Args:
+            token_params (Dict): key, value param for token object creation.
+
+        """
+        try:
+            token_params['uuid'] = str(uuid.uuid4())
+            current_token_instance = Token(**token_params)
+            self.session_instance.add(current_token_instance)
+            self.session_instance.commit()
+            return True
+        except Exception as error:
+            print('HandleDB::create_token:{}'.format(error.message))
             return False
