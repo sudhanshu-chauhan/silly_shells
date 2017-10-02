b5c0b32be1eb811e3d89384e4eeadf54c6b12e90
sudhanshu singh chauhan <minimalcrap@gmail.com>
2017-10-02 23:20:55 +0530

    modify db_handler.py to add update functions.

--- components/lib/db_handler.py
+++ components/lib/db_handler.py
@@ -4,7 +4,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 from models import (User,
-                    SecurityCredential)
+                    SecurityCredential,
+                    ClientMachine,
+                    ClientProcess)
 from conf.settings import DATABASE_URL
 from logger import Logger
 
@@ -52,6 +54,21 @@ class HandleDB:
                 'HandleDB::create_user:{}'.format(error.message))
             return False
 
+    def update_user(self, user_id, **user_params):
+        """Update User method for HandleDB."""
+        try:
+            self.session_instance.query(User).filter_by(
+                uuid=user_id).update(**user_params)
+            self.session_instance.commit()
+            return self.session_instance.query(
+                User).filter_by(uuid=user_id).first()
+
+        except Exception as error:
+            logger_instance.logger.error(
+                'HandleDB::update_user:{}'.format(
+                    error.message))
+            return None
+
     def create_security_credential(self, **secureity_credential_params):
         """Create Security Credential method for HandleDB Class.
 
@@ -70,7 +87,94 @@ class HandleDB:
             return security_credential_instance
 
         except Exception as error:
-            logger_instance.logger.error('HandleDB::create_security_credential:{}'.format(
-                error.message
-            ))
+            logger_instance.logger.error(
+                'HandleDB::create_security_credential:{}'.format(
+                    error.message
+                ))
             return False
+
+    def update_security_credential(self,
+                                   security_credential_id,
+                                   **security_credential_params):
+        """Update Security Credential method for HandleDB class."""
+        try:
+            self.session_instance.query(
+                SecurityCredential).filter_by(
+                uuid=security_credential_id).update(
+                **security_credential_params)
+            self.session_instance.commit()
+            return self.session_instance.query(
+                SecurityCredential).filter_by(
+                uuid=security_credential_id).first()
+
+        except Exception as error:
+            logger_instance.logger.error(
+                'HandleDB::update_security_credentials:{}'.format(
+                    error.message))
+            return None
+
+    def create_client_machine(self, **client_machine_params):
+        """Create Client Machine method for HandleDB Class."""
+        try:
+            client_machine_params['uuid'] = str(uuid.uuid4())
+            client_machine_instance = ClientMachine(**client_machine_params)
+            self.session_instance.add(client_machine_instance)
+            self.session_instance.commit()
+            return client_machine_instance
+        except Exception as error:
+            logger_instance.logger.error(
+                'HandleDB::create_client_machine:{}'.format(
+                    error.message))
+            return None
+
+    def update_client_machine(self,
+                              client_machine_id,
+                              **client_machine_params):
+        """Update Client Machine method for HandleDB class."""
+        try:
+            self.session_instance.query(
+                ClientMachine).filter_by(
+                uuid=client_machine_id).update(**client_machine_params)
+            self.session_instance.commit()
+            return self.session_instance.query(
+                ClientMachine).filter_by(
+                uuid=client_machine_id).first()
+
+        except Exception as error:
+            logger_instance.logger.error(
+                'HandleDB::update_client_machine:{}'.format(
+                    error.message))
+            return None
+
+    def create_client_process(self, **client_process_params):
+        """Create Client Process method for HandleDB class."""
+        try:
+            client_process_params['uuid'] = str(uuid.uuid4())
+            client_process_instance = ClientProcess(**client_process_params)
+            self.session_instance.add(client_process_instance)
+            self.session_instance.commit()
+            return client_process_instance
+
+        except Exception as error:
+            logger_instance.logger.error(
+                'HandleDB::create_client_process:{}'.format(
+                    error.message))
+            return None
+
+    def update_client_process(self,
+                              client_process_id,
+                              **client_process_params):
+        """Update Client Process method for HandleDB class."""
+        try:
+            self.session_instance.query(
+                ClientProcess).filter_by(uuid=client_process_id).update(
+                **client_process_params)
+            self.session_instance.commit()
+            return self.session_instance.query(
+                ClientProcess).filter_by(uuid=client_process_id).first()
+
+        except Exception as error:
+            logger_instance.logger.error(
+                'HandleDB::update_client_process:{}'.format(
+                    error.message))
+            return None
