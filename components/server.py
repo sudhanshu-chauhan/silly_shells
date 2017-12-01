c1be7b127a43bda129b37449c863f4f59ac774f6
sudhanshu singh chauhan <minimalcrap@gmail.com>
2017-12-01 23:08:21 +0530

    feat.(silly_shells): modify server.py to add Client Machine Request Handler.

--- components/server.py
+++ components/server.py
@@ -6,19 +6,17 @@ from tornado.web import Application, RequestHandler
 import jwt
 
 from lib.logger import Logger
-from lib.controller import UserController
+from lib.controller import UserController, ClientMachineController
 from lib.conf import settings
 
 logger_instance = Logger(**{
     'file_name': 'error.log',
     'stream_handler': True,
-    'file_handler': True
-})
+    'file_handler': True})
 logger_instance_info = Logger(**{
     'file_name': 'tornado_server.log',
     'stream_handler': True,
-    'file_handler': False
-})
+    'file_handler': False})
 
 
 active_clients = []
@@ -45,8 +43,7 @@ class SocketOutputHandler(WebSocketHandler):
         except Exception as error:
             logger_instance.logger.error(
                 'SocketOutputHandler::open:{}'.format(
-                    error.message
-                ))
+                    error.message))
 
     def on_message(self, message):
         try:
@@ -68,7 +65,9 @@ class SocketOutputHandler(WebSocketHandler):
         try:
             pass
         except Exception as error:
-            logger_instance
+            logger_instance.logger.error(
+                'SocketOutputHandler::authenticate_client:{}'.format(
+                    error.message))
 
 
 class ClientAuthentication(RequestHandler):
@@ -83,8 +82,7 @@ class ClientAuthentication(RequestHandler):
                                            post_param['password']):
                 payload = {
                     'email': post_param['email'],
-                    'password': post_param['password']
-                }
+                    'password': post_param['password']}
                 response_json = {'token': jwt.encode(
                     payload, settings.JWT_SECRET, algorithm='HS512')}
                 self.write(json.dumps(response_json))
@@ -98,13 +96,43 @@ class ClientAuthentication(RequestHandler):
                 'ClientAuthentication::post:{}'.format(error.message))
 
 
+class Machine(RequestHandler):
+    """Machine class to create/list machine instances."""
+
+    def post(self, *args, **kwargs):
+        """Post method to accept params and create a machine instance."""
+        try:
+            post_param = json.loads(self.request.body)
+            token = self.request.headers.get('Authorization')
+            token_payload = jwt.decode(token, verify=False)
+            if 'email' in token_payload:
+                machine_id = ClientMachineController.create_client_machine(
+                    **post_param)
+                if machine_id is not None:
+                    self.clear()
+                    self.set_status(201)
+                    self.write(machine_id)
+                else:
+                    self.clear()
+                    self.set_status(500)
+                    self.write('<html><body>server error: 500</body></html>')
+            else:
+                self.clear()
+                self.set_status(403)
+                self.write('<html><body>invalid token</body></html>')
+
+        except Exception as error:
+            logger_instance.logger.error(
+                'Machine::post:{}'.format(error.message))
+
+
 class MainApplication(Application):
 
     def __init__(self):
         handlers = [
             (r'/sock_server/', SocketOutputHandler),
             (r'/get_token/', ClientAuthentication),
-        ]
+            (r'/create_machine/', Machine)]
 
         Application.__init__(self, handlers)
 
