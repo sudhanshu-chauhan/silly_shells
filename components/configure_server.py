bc55a3c7c614a19cc01804fc6a02792b20f8a626
sudhanshu singh chauhan <minimalcrap@gmail.com>
2017-12-02 23:19:35 +0530

    feat.(silly_shells): modify configure_server script to allow database and model creation.

--- components/configure_server.py
+++ components/configure_server.py
@@ -3,11 +3,18 @@ import sys
 
 import psycopg2
 import ConfigParser
+from lib import models
 
 
 def main():
     try:
         config_file_path = os.environ.get('CONFIG_FILE_PATH')
+        config_instance = ConfigParser()
+        if not os.path.exists(config_file_path):
+            print('configuration file not found...exiting')
+            sys.exit(1)
+
+        config_instance.read()
         postgres_db_params = {}
         print('configuring silly shells server...')
         print('configuring postgres db params')
@@ -26,8 +33,14 @@ def main():
         print('checking postgresql database params...')
         connection = psycopg2.connect(**postgres_db_params)
         print('postgresql params verification successful!')
+        print('Creating database and models...')
+        cursor = connection.cursor()
+        cursor.execute('create database {}'.format(
+            postgres_db_params['dbname']))
+        cursor.close()
         connection.close()
-
+        models.Base.metadata.create_all(models.engine)
+        print('Database models created successfully!')
 
     except Exception as error:
         print(error.message)
