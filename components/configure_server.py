1e4693fb104bbade6edc2e85642b762f705cce25
sudhanshu singh chauhan <minimalcrap@gmail.com>
2017-12-04 23:15:48 +0530

    feat.(silly_shells): modify configure_server.py to add mongo server status check

--- components/configure_server.py
+++ components/configure_server.py
@@ -2,21 +2,29 @@ import os
 import sys
 
 import psycopg2
-import ConfigParser
+from ConfigParser import ConfigParser
 from lib import models
+
+from pymongo import MongoClient
+from pymongo.errors import ServerSelectionTimeoutError
+
+MAX_SERVER_DELAY = 10  # server timeout value in microseconds
 
 
 def main():
     try:
-        config_file_path = os.environ.get('CONFIG_FILE_PATH')
+        config_file_path = os.environ.get('SERVER_CONFIG_FILE_PATH')
         config_instance = ConfigParser()
         if not os.path.exists(config_file_path):
             print('configuration file not found...exiting')
             sys.exit(1)
 
-        config_instance.read()
+        config_instance.read(config_file_path)
         postgres_db_params = {}
         print('configuring silly shells server...')
+        print('checking mongodb server...')
+        mongo_client = MongoClient(serverSelectionTimeoutMS=MAX_SERVER_DELAY)
+        mongo_client.server_info()
         print('configuring postgres db params')
 
         postgres_db_params['host'] = str(
@@ -42,6 +50,15 @@ def main():
         models.Base.metadata.create_all(models.engine)
         print('Database models created successfully!')
 
-    except Exception as error:
-        print(error.message)
+    except ServerSelectionTimeoutError as mongo_error:
+        print('mongodb server error: {}'.format(mongo_error.message))
+        print('server configuration failed. exiting ...')
         sys.exit(1)
+
+    except Exception as error:
+        print(error)
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
